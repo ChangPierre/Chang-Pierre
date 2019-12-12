80e400899a8bbbd994859d5f5134f84f9f62bbaf
ChangPierre <58828961+ChangPierre@users.noreply.github.com>
2019-12-12 16:15:28 -0500

    Update conection.js

--- conection.js
+++ conection.js
@@ -1,11 +1,11 @@
 const {Pool} = require("pg");
 
 const pool = new Pool({
-	host: 'ec2-174-129-238-192.compute-1.amazonaws.com',
-	database: 'da87k703ao25h2',
-	user: 'fboprjnznuvwwr',
+	host: 'ec2-174-129-255-99.compute-1.amazonaws.com',
+	database: 'd23c6betn6i87q',
+	user: 'gpmxpimbwisble',
 	port: 5432,
-	password: '08946a6bb897fb2443d88c8bcdd34c6fa95574a8dd31e1869fb233c10f8a6010',
+	password: 'ee3d151b47a6af0246f0904acf74ef55b9b0feaac209c88ef09f5960754254d6',
 	ssl: true
 });
 module.exports = pool;
